0ba7629beb9e866cef814afd2ec00d7b94388b8b
qingqingsu <73687188+qingqingsu@users.noreply.github.com>
2020-12-14 18:57:46 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import App from './App'
+import '@/static/fonts/iconfont.css'
+import '@/static/font_2269866_wp049iq93mj/iconfont.css'
+import '@/static/font_2269987_04jna0qkj44z/iconfont.css'
+import '@/static/font_2269997_0gw2sqcvvpqn/iconfont.css'
+import '@/static/font_2270004_60akpa6aly/iconfont.css'
+Vue.config.productionTip = false
+
+App.mpType = 'app'
+
+const app = new Vue({
+	...App
+})
+app.$mount()
---
+++ utils/zgrequest.js
@@ -0,0 +1,41 @@
+const baseUrl = "https://uptownlet.com"
+
+export function myRequestGet(url, data) {
+	return new Promise((resolve, reject) => {
+		uni.request({
+			url: baseUrl + url,
+			method: "GET",
+			data: data,
+			success: function(res) {
+				resolve(res.data)
+			},
+			fail: function(err) {
+				reject(err)
+			}
+		})
+	})
+}
+
+export function myRequestPost(url, data) {
+	return new Promise((resolve, reject) => {
+		uni.request({
+			url: baseUrl + url,
+			header: {
+				'Content-Type': 'application/x-www-form-urlencoded'
+			},
+			method: "POST",
+			data: data,
+			success: function(res) {
+				resolve(res.data)
+				console.log("收到数据");
+			},
+			fail: function(err) {
+				reject(err)
+			},
+		})
+
+	}).catch((e) => {
+		console.log("11134567");
+	});
+
+}
